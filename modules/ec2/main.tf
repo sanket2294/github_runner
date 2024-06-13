5e938c346a4c812dcccfc39bc607a5108de508d2
Sanket Avhad <sanketa2@birlasoft.com>
2024-06-13 22:59:06 +0530

    Remove duplicate variable declarations from main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,38 +1,3 @@
-variable "ami" {
-  description = "AMI ID"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Key pair name"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet ID"
-  type        = string
-}
-
-variable "tags" {
-  description = "Tags to apply to the instance"
-  type        = map(string)
-}
-
-variable "user_data" {
-  description = "User data script"
-  type        = string
-}
-
 resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
