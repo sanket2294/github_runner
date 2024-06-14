9cec599b6cc8cfb9f32f17ea2275230811478710
Sanket Avhad <sanketa2@birlasoft.com>
2024-06-14 13:35:30 +0530

    updating the us-east-2 region and AMI ID

--- variables.tf
+++ variables.tf
@@ -42,5 +42,5 @@ variable "vpc_id" {
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
